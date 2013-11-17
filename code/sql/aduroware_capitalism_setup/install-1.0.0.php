403d8a40fe7699d4df4fd99cba4d55f7920dae75
Jake Shelby <jake.shelby@me.com>
2013-11-17 12:55:06 -0700

    Added link to user id

--- code/sql/aduroware_capitalism_setup/install-1.0.0.php
+++ code/sql/aduroware_capitalism_setup/install-1.0.0.php
@@ -7,12 +7,17 @@ $installer->startSetup();
 // Create table 'capitalism/player'
 $table = $installer->getConnection()
     ->newTable($installer->getTable('capitalism/player'))
-    ->addColumn('player_id', Varien_Db_Ddl_Table::TYPE_BIGINT, null, array(
+    ->addColumn('player_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
         'identity'  => true,
         'nullable'  => false,
         'primary'   => true,
         'unsigned'  => true,
         ), 'Player ID')
+    ->addColumn('user_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+        ), 'User ID')
     ->addColumn('username', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
         'nullable'  => false,
         ), 'User Name')
@@ -28,22 +33,29 @@ $table = $installer->getConnection()
         ), 'Player Created Date')
     ->addColumn('updated_date', Varien_Db_Ddl_Table::TYPE_TIMESTAMP, null, array(
         ), 'Player Created Date')
+    ->addIndex(
+        $installer->getIdxName('capitalism/player', 'user_id', Varien_Db_Adapter_Interface::INDEX_TYPE_UNIQUE),
+        'user_id',
+        array('type'=>Varien_Db_Adapter_Interface::INDEX_TYPE_UNIQUE))
+    ->addForeignKey($installer->getFkName('capitalism/player', 'user_id', 'user/entity', 'entity_id'),
+        'user_id', $installer->getTable('user/entity'), 'entity_id',
+        Varien_Db_Ddl_Table::ACTION_CASCADE, Varien_Db_Ddl_Table::ACTION_CASCADE)
     ->setComment('Capitalism Player Table');
 $installer->getConnection()->createTable($table);
 
 // Create table 'capitalism/player_transaction'
 $table = $installer->getConnection()
     ->newTable($installer->getTable('capitalism/player_transaction'))
-    ->addColumn('player_transaction_id', Varien_Db_Ddl_Table::TYPE_BIGINT, null, array(
+    ->addColumn('player_transaction_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
         'identity'  => true,
         'nullable'  => false,
         'primary'   => true,
         ), 'Player Transaction ID')
-    ->addColumn('giving_player_id', Varien_Db_Ddl_Table::TYPE_BIGINT, null, array(
+    ->addColumn('giving_player_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
         'nullable'  => false,
         'unsigned'  => true,
         ), 'Giving Player ID')
-    ->addColumn('receiving_player_id', Varien_Db_Ddl_Table::TYPE_BIGINT, null, array(
+    ->addColumn('receiving_player_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
         'nullable'  => false,
         'unsigned'  => true,
         ), 'Receiving Player ID')
