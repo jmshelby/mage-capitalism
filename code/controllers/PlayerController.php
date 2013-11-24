b1b9a72841f480274cf250158694ab67884ed948
Jake Shelby <jake.shelby@me.com>
2013-11-23 23:59:31 -0700

    cleanup

--- code/controllers/PlayerController.php
+++ code/controllers/PlayerController.php
@@ -39,34 +39,4 @@ class Aduroware_Capitalism_PlayerController extends Mage_Core_Controller_Front_A
 		$this->_redirect('*/index/');
 	}
 
-	public function enrollAction()
-	{
-
-		$hlp = Mage::helper('capitalism');
-		$session = Mage::getSingleton('user/session');
-		$newUsername = $this->getRequest()->getParam('new_username');
-
-		if ($hlp->getCurrentPlayer()) {
-			$session->addError("You're already enrolled.");
-			$this->_redirect('*/');
-			return;
-		}
-
-		if ($hlp->isUsernameTaken($newUsername)) {
-			$session->addError("Username already taken, try again.");
-			$this->_redirect('*/');
-			return;
-		}
-
-		try {
-			$newPlayer = $hlp->getCurrentPlayer($newUsername);
-			$session->addSuccess("Account created!");
-		} catch (Exception $e) {
-			$session->addError("An error occurred while trying to create your account: ".$e->getMessage());
-		}
-
-		$this->_redirect('*/');
-
-	}
-
 }
